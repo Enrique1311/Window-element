158b8eb93dc42f5781d99346e96f6cca874a8bd7
Enrique1311 <ejspinelli131176@gmail.com>
2024-06-13 15:12:46 -0300

    Working...

--- Exercise-1/js/main.js
+++ Exercise-1/js/main.js
@@ -7,23 +7,39 @@ const students = [
 	{ name: "Amalia", email: "amalia@gmail.com", subject: "Language" },
 ];
 
-const $gridContainer = document.querySelector(".grid-container");
+const $gridContainer = document.querySelector(".grid-container"),
+	$button = document.querySelector("button");
 
 students.forEach((student) => {
 	let htmlCode = `
     			<h3 class="grid-item">${student.name}</h3>
 			<p class="grid-item">${student.email}</p>
 			<p class="grid-item">${student.subject}</p>
-			<div class="grid-item">
+			<div class="grid-item week">
 				<select
-					name=""
-					id=""
+					class="choosen-week"
 				>
-					<option>Week 1</option>
-					<option>Week 2</option>
+					<option value="Week 1">Week 1</option>
+					<option value="Week 2">Week 2</option>
 				</select>
 			</div>
     `;
 
 	$gridContainer.innerHTML += htmlCode;
+});
+
+$button.addEventListener("click", () => {
+	let $elements = document.querySelectorAll(".week"),
+		$choosenWeeks = document.querySelectorAll(".choosen-week");
+
+	// console.log($elements, $choosenWeeks);
+
+	// $elements.forEach((el) => {
+	// 	el.innerHTML = $choosenWeeks[el].value;
+	// });
+
+	for (el in $elements) {
+		let week = $elements[el];
+		week.innerHTML = $choosenWeeks[el].value;
+	}
 });
